23d39f9f1c368c296ca37bf328549770e13577e1
Vardges-Khachatryan <khachatryanvardges95@gmail.com>
2022-09-14 09:31:29 +0400

    Add PrimitiveTypes

--- src/PrimitiveTypes.java
+++ src/PrimitiveTypes.java
@@ -1,2 +1,50 @@
 public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        int i = 15;
+        int i1 = -32000000;
+        int i2 = 0;
+        int i3 = 0B0110011;
+
+        long l = 2467784839398847575L;
+        long l1 = i1;
+        long l2 = -25987452;
+
+        float f = 154;
+        float f1 = 258.36F;
+
+        double d = 154;
+        double d1 = 125.59;
+
+        char ch = 'X';
+        char ch1 = '8';
+        char ch2 = 6400;
+
+        boolean b = true;
+        boolean b1 = false;
+        boolean b2 = 4 == 4;
+        boolean b3 = 5 < 1;
+
+        System.out.println("i is " + i);
+        System.out.println("i1 is " + i1);
+        System.out.println("i2 is " + i2);
+        System.out.println("i3 is " + i3);
+        System.out.println("l is " + l);
+        System.out.println("l1 is " + l1);
+        System.out.println("l2 is " + l2);
+        System.out.println("f is " + f);
+        System.out.println("f1 is " + f1);
+        System.out.println("d is " + d);
+        System.out.println("d1 is " + d1);
+        System.out.println("ch is " + ch);
+        System.out.println("ch1 is " + ch1);
+        System.out.println("ch2 is " + ch2);
+        System.out.println("b is " + b);
+        System.out.println("b1 is " + b1);
+        System.out.println("b2 is " + b2);
+        System.out.println("b3 is " + b3);
+
+    }
+
+
 }
